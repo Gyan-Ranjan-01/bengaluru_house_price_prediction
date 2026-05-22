63613f35316f0a4dd37b7e5d5238a2b174a45b40
Gyan Ranjan <sunbabuwa00@gmail.com>
2026-05-22 20:56:27 +0530

    Production Ready

--- main.py
+++ main.py
@@ -41,5 +41,11 @@ def predict():
     except Exception as e:
         return jsonify({'success': False, 'err' : str(e)}),400
 
-if __name__=="__main__":
-    app.run(debug=True)
+# if __name__=="__main__":
+#     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
